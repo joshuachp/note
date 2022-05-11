7c64ce041e3f238402252a1120dedad95debcad5
Joshua Chapman <joshua.samuel.chapman@protonmail.com>
2022-05-11 13:55:31 +0200

    feat: add nix files

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    };
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    naersk = {
+      url = "github:nmattia/naersk/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    naersk,
+    fenix,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      naersk-lib = pkgs.callPackage naersk {};
+    in rec {
+      packages.default =
+        (naersk-lib.override {
+          inherit (fenix.packages.${system}.stable) cargo rustc;
+        })
+        .buildPackage {
+          root = ./.;
+        };
+
+      app.default = flake-utils.lib.mkApp {
+        drv = packages.default;
+      };
+
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          (fenix.packages.${system}.stable.withComponents [
+            "cargo"
+            "clippy"
+            "rust-src"
+            "rustc"
+            "rustfmt"
+          ])
+          pre-commit
+        ];
+      };
+    });
+}
