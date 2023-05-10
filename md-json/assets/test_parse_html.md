f340399aa25f2d58fad797145f77b8c73984f14b
Joshua Chapman <joshua.chapman@joshuachp.dev>
2023-05-10 21:23:28 +0200

    WIP: parse html

---
+++ md-json/assets/test_parse_html.md
@@ -0,0 +1,17 @@
+---
+title: "parse_html"
+tags: []
+date: 2023-02-19
+---
+
+# Parse html
+
+Some test
+
+<details>
+
+Some details
+
+</details>
+
+Other test.
--- md-json/src/parser.rs
+++ md-json/src/parser.rs
@@ -1,4 +1,5 @@
 use chrono::NaiveDate;
+use html_parse::html::Html;
 use nom::{
     branch::alt,
     bytes::complete::{tag, take_until},
@@ -7,7 +8,7 @@ use nom::{
     sequence::{delimited, pair},
     IResult,
 };
-use pulldown_cmark::{Event, Options, Parser};
+use pulldown_cmark::{CowStr, Event, Options, Parser, Tag};
 use serde::{Deserialize, Serialize};
 
 use crate::error::Error;
@@ -20,7 +21,48 @@ pub struct Markdown<'a> {
     pub date: NaiveDate,
     pub draft: bool,
     pub language: Option<String>,
-    pub content: Vec<Event<'a>>,
+    pub content: Vec<MdEvent<'a>>,
+}
+
+#[derive(Serialize, Debug)]
+pub enum MdEvent<'a> {
+    #[cfg_attr(feature = "serde", serde(borrow))]
+    Start(Tag<'a>),
+    #[cfg_attr(feature = "serde", serde(borrow))]
+    End(Tag<'a>),
+    #[cfg_attr(feature = "serde", serde(borrow))]
+    Text(CowStr<'a>),
+    #[cfg_attr(feature = "serde", serde(borrow))]
+    Code(CowStr<'a>),
+    #[cfg_attr(feature = "serde", serde(borrow))]
+    Html(Html),
+    #[cfg_attr(feature = "serde", serde(borrow))]
+    FootnoteReference(CowStr<'a>),
+    SoftBreak,
+    HardBreak,
+    Rule,
+    TaskListMarker(bool),
+}
+
+impl<'a> From<Event<'a>> for MdEvent<'a> {
+    fn from(value: Event<'a>) -> Self {
+        match value {
+            Event::Start(value) => MdEvent::Start(value),
+            Event::End(value) => MdEvent::End(value),
+            Event::Text(value) => MdEvent::Text(value),
+            Event::Code(value) => MdEvent::Code(value),
+            Event::FootnoteReference(value) => MdEvent::FootnoteReference(value),
+            Event::SoftBreak => MdEvent::SoftBreak,
+            Event::HardBreak => MdEvent::HardBreak,
+            Event::Rule => MdEvent::Rule,
+            Event::TaskListMarker(value) => MdEvent::TaskListMarker(value),
+            Event::Html(fragment) => {
+                let html = Html::parse_fragment(&fragment);
+
+                MdEvent::Html(html)
+            }
+        }
+    }
 }
 
 #[derive(Deserialize, Debug)]
@@ -67,7 +109,7 @@ pub fn parse(markdown: &str) -> Result<Markdown, Error> {
     let date = NaiveDate::parse_from_str(&date, "%Y-%m-%d").map_err(Error::Date)?;
 
     let options = Options::all();
-    let parser = Parser::new_ext(content, options);
+    let parser = Parser::new_ext(content, options).map(MdEvent::from);
 
     Ok(Markdown {
         title,
@@ -82,7 +124,7 @@ pub fn parse(markdown: &str) -> Result<Markdown, Error> {
 
 #[cfg(test)]
 mod test {
-    use super::front_matter_parser;
+    use super::{front_matter_parser, parse};
 
     #[test]
     fn should_parse_front_matter() {
@@ -95,5 +137,16 @@ title: "some"
         let result = front_matter_parser(markdown);
 
         assert_eq!(result, Ok(("\n# Hello world", "title: \"some\"\n")));
+    }
+
+    #[test]
+    fn should_parse_html() {
+        let html_test = include_str!("../assets/test_parse_html.md");
+
+        let result = parse(html_test);
+
+        dbg!(result.unwrap());
+
+        panic!()
     }
 }
