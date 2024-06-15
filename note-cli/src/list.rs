60a7c51b365eba7c84fe07f5b81fdc7288003e5f
Joshua Chapman <joshua.chapman@joshuachp.dev>
2024-06-15 11:02:20 +0200

    feat(list): improve the listing logic

--- note-cli/src/list.rs
+++ note-cli/src/list.rs
@@ -1,9 +1,9 @@
 use std::{
-    fs::{self},
-    path::PathBuf,
+    fs,
+    path::{Path, PathBuf},
 };
 
-use color_eyre::eyre::Context;
+use color_eyre::eyre::{Context, OptionExt};
 use log::debug;
 use walkdir::{DirEntry, WalkDir};
 
@@ -22,30 +22,59 @@ pub fn list_path(
     path: Option<PathBuf>,
     max_depth: usize,
 ) -> color_eyre::Result<()> {
+    debug!("input path {:?} with max_depth {max_depth}", path);
+
     let path = get_path_to_list(config, path).wrap_err("couldn't get the path to list")?;
 
-    let entries = WalkDir::new(path)
+    debug!("chosen path {}", path.display());
+
+    let entries = WalkDir::new(&path)
         .max_depth(max_depth)
         .follow_links(true)
+        .sort_by(|a, b| {
+            // Files first, then compare by name
+            a.file_type()
+                .is_file()
+                .cmp(&b.file_type().is_file())
+                // file (true) is greater than dir (false) so we reverse here
+                .reverse()
+                .then_with(|| a.file_name().cmp(b.file_name()))
+        })
         .into_iter()
-        .filter_entry(|e| !is_hidden(e))
+        .filter_entry(|e| {
+            debug!("checking entry {}", e.path().display());
+
+            !is_hidden(e)
+        })
         .collect::<Result<Vec<DirEntry>, _>>()
         .wrap_err("couldn't read entry")?;
 
     for entry in entries {
-        print_entry(entry)?;
+        debug!("entry {}", entry.path().display());
+
+        if entry.path() == path {
+            debug!("skipping current");
+
+            continue;
+        }
+
+        print_entry(entry, &config.note_path)?;
     }
 
     Ok(())
 }
 
-fn print_entry(entry: DirEntry) -> color_eyre::Result<()> {
+fn print_entry(entry: DirEntry, note_path: &Path) -> color_eyre::Result<()> {
     let metadata = entry
         .metadata()
         .wrap_err_with(|| format!("couldn't read metadata for {}", entry.path().display()))?;
 
     if metadata.is_dir() {
-        println!("{}", entry.path().display());
+        let path = strip_note_prefix(note_path, entry.path())?;
+
+        println!("{}/", path.display());
+
+        return Ok(());
     }
 
     if !entry.path().extension().is_some_and(|ext| ext == "md") {
@@ -60,25 +89,54 @@ fn print_entry(entry: DirEntry) -> color_eyre::Result<()> {
     let note = md_parser::parse(&content)
         .wrap_err_with(|| format!("couldn't parse {}", entry.path().display()))?;
 
-    println!("{}\t{}", entry.path().display(), note.title);
+    let path = strip_note_prefix(note_path, entry.path())?;
+
+    println!("{}\t{}", path.display(), note.title);
 
     Ok(())
 }
 
+fn strip_note_prefix<'a>(note_path: &'a Path, path: &'a Path) -> color_eyre::Result<&'a Path> {
+    if path.starts_with(note_path) {
+        return path
+            .strip_prefix(note_path)
+            .wrap_err_with(|| format!("couldn't strip note path prefix from {}", path.display()));
+    }
+
+    Ok(path)
+}
+
 fn get_path_to_list(config: &Config, path: Option<PathBuf>) -> color_eyre::Result<PathBuf> {
-    if let Some(path) = path {
-        return Ok(path);
+    let Some(path) = path else {
+        return Ok(config.note_path.clone());
+    };
+
+    if path.is_absolute() {
+        return path
+            .canonicalize()
+            .wrap_err_with(|| format!("couldn't get absolute path {}", path.display()));
     }
 
-    let note_path = &config.note_path;
+    let local = path
+        .try_exists()
+        .wrap_err_with(|| format!("couldn't read relative path {}", path.display()))?;
 
-    let cwd = std::env::current_dir()
-        .and_then(|dir| dir.canonicalize())
-        .wrap_err("couldn't get CWD")?;
-
-    if cwd.starts_with(note_path) {
-        return Ok(cwd);
+    if local {
+        return Ok(path.to_owned());
     }
 
-    Ok(note_path.to_owned())
+    let mut note_path = config.note_path.join(&path);
+
+    let exists = note_path
+        .try_exists()
+        .wrap_err_with(|| format!("couldn't read note path {}", note_path.display()))?;
+
+    if exists {
+        return Ok(note_path);
+    }
+
+    note_path
+        .pop()
+        .then_some(note_path)
+        .ok_or_eyre("not a valid path")
 }
